fa64d9e766612ef6ac386ee0ae7d546ee88e5fec
uquokka <107118528+uquokka@users.noreply.github.com>
2022-06-08 21:25:34 +0900

    웹보안실습용

---
+++ idpassword.php
@@ -0,0 +1,20 @@
+<?php
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset = "ut f-8">
+        <title>HTML</title>
+        <style>
+            *{ font-size:16px;
+            font-family: Consolas, sans-serif;
+        }
+        </style>
+        </head>
+        <body>
+            <p> ID : <?php echo $email >.</p>
+            <p> PASSWORD : <?php echo $pass>.</p>
+            </body>
+            </html>
